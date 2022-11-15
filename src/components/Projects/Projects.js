0fce3d373af95aa4b91c9e2b1187275b1479843a
Tejinderb <78414327+Tejinderb@users.noreply.github.com>
2022-11-15 19:53:18 +0530

    Add files via upload

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import chatify from "../../Assets/Projects/chatify.png";
+
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Travel Advisor"
+              description="Travel website which tells you about the exotic location there and it gives you packages regarding your trip planning."
+              ghLink="https://github.com/iananyaverma"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
